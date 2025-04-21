54b804f539b460b29c28832c9e7609f051e403bb
David Bautista <dj.bautista10@uniandes.edu.co>
2025-04-21 10:00:59 -0500

    asdkaskd

--- app/roller/layout.tsx
+++ app/roller/layout.tsx
@@ -2,10 +2,10 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "¡Nos vamos al Parque de Atracciones!",
-  description: "Una sorpresa especial te espera al final del juego.",
+  description: "Vamos a compartir un día increíble para celebrar el cumpleaños de Andrea.",
   openGraph: {
     title: "¡Nos vamos al Parque de Atracciones!",
-    description: "Una sorpresa especial te espera al final del juego.",
+    description: "Vamos a compartir un día increíble para celebrar el cumpleaños de Andrea.",
     images: ["/og.png"]
   }
 };
